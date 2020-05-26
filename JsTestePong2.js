997465a2f5e17efc5297139644abc2015ef6f409
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-25 21:00:05 -0300

    Add field printing

--- JsTestePong2.js
+++ JsTestePong2.js
@@ -1,13 +1,14 @@
 
 window.onload = ()=> {
   let my = setup_drawing_tools()
+  let screen_width = window.innerWidth;
+  let half_screen_width = screen_width/2;
+  let screen_height = window.innerHeight;
   fix_canvas_proportions(my.canvas)
 
-  //test set
-  //  set_brush_color(my.brush ,'#45A05A');
-  //  draw_rectangle({brush: my.brush, x_axis: 15, y_axis: 25, width: 500, height: 100});
-  //  set_brush_font({brush: my.brush, font_size: 40, font_family: 'Helvetica'})
-  //  write_text({brush: my.brush, x_axis: 50, y_axis: 150,text: 'test text!'})
+  draw_field({brush: my.brush, field_color: '#081605',
+              lines_color: '#FFFFFF', width: screen_width,
+              height: screen_height, half: half_screen_width})
 }
 
 const log = (what = '', where = 'console')=> {
@@ -18,8 +19,8 @@ const log = (what = '', where = 'console')=> {
 }
 
 let fix_canvas_proportions = (canvas)=> {
-  canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
 const setup_drawing_tools = ()=> {
@@ -44,4 +45,15 @@ let set_brush_font = ({brush, font_size, font_family})=> {
 
 let write_text = ({brush,  x_axis, y_axis, text})=> {
   brush.fillText(text, x_axis, y_axis);
+}
+
+function draw_field({brush, field_color, lines_color, width, height, half}){
+
+  set_brush_color(brush,field_color);
+  draw_rectangle({brush: brush, x_axis: 0, y_axis: 0,
+                  width: width, height: height});
+
+  set_brush_color(brush,lines_color);
+  draw_rectangle({brush: brush, x_axis: half - 5, y_axis: 0,
+                  width: 10, height: height});
 }
