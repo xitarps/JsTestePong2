5855f1cc7dd221966c654b73c61d1d891e87f90c
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-26 18:41:13 -0300

    Add ball move

--- JsTestePong2.js
+++ JsTestePong2.js
@@ -1,22 +1,23 @@
 
 window.onload = ()=> {
-  let my = setup_drawing_tools()
+  let my = setup_drawing_tools();
   let screen_width = window.innerWidth;
   let half_screen_width = screen_width/2;
   let screen_height = window.innerHeight;
   let fps = 30;
-  let player = {y_position: 300}
-  let computer = {y_position: 300}
-  let ball = {x_axis: half_screen_width-5, y_axis: screen_height/2}
-  fix_canvas_proportions(my.canvas)
+  let player = {y_position: 300};
+  let computer = {y_position: 300};
+  let ball = {x_axis: half_screen_width-5, y_axis: screen_height/2,
+              speed: 100, x_direction: -1, y_direction: 1};
+  fix_canvas_proportions(my.canvas);
 
 
 
-  track_player_move(player)
-
+  track_player_move(player);
+  ball_move(ball);
   render_game_screen({brush: my.brush, fps, player, computer, ball,
                       screen_width, screen_height,
-                      half_screen_width})
+                      half_screen_width});
 }
 
 const log = (what = '', where = 'console')=> {
@@ -84,11 +85,18 @@ let track_player_move = (player)=> {
   });
 }
 
+let ball_move = (ball)=> {
+  setInterval(() => {
+    
+    ball.x_axis = ball.x_axis + ball.x_direction
+    ball.y_axis = ball.y_axis + ball.y_direction
+
+  }, 1000/ball.speed );
+}
+
 let render_game_screen = ({brush, fps, player, ball,
                            screen_width, screen_height, half_screen_width})=> {
   setInterval(() => {
-
-    
 
     draw_field({brush, field_color: '#081605',
                 lines_color: '#FFFFFF', width: screen_width,
@@ -104,12 +112,13 @@ let render_game_screen = ({brush, fps, player, ball,
                   x_axis: screen_width - 15, y_axis: 300,
                   width: 15, height: 60});
 
-    //computer
+    //ball
     draw_ball({brush, color:'#FFFFFF', 
                x_axis: ball.x_axis, y_axis: ball.y_axis,
                width: 10, height: 10});
 
     set_brush_font({brush, font_size: 1.5, font_family: 'Quantico' })
     write_text({brush,  x_axis: 20, y_axis: 30, text: `${fps} fps`})
+
   }, 1000/fps);
 }
