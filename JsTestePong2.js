e053cf86a4178c9f02283191fcb275e26e20af22
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-27 19:47:59 -0300

    Add difficulty variable to adjust computer speed
    In future work will add dynamic adjust of this speed due some plays
    'crash' the 'i.a'

--- JsTestePong2.js
+++ JsTestePong2.js
@@ -8,7 +8,8 @@ window.onload = ()=> {
   const flipper_size = 60;
   let score = {player: 0, computer: 0, max: 5};
   let player = {y_position: 300};
-  let computer = {y_position: 50};
+  let computer = {y_position: 400};
+  let dificulty = 3;
   let start_direction = random_start_direction();
   let ball = {x_axis: half_screen_width-5, y_axis: screen_height/2, speed: 150,
               size: 10,
@@ -19,6 +20,7 @@ window.onload = ()=> {
 
 
   track_player_move(player);
+  track_computer_move(computer, ball, flipper_size, dificulty);
   ball_move(ball);
   check_ball_bounce(ball, flipper_size, screen_height,screen_width,player,
                     computer, score);
@@ -92,6 +94,20 @@ let track_player_move = (player)=> {
     player.y_position = event.clientY;
   });
 }
+
+let track_computer_move = (computer, ball, flipper_size, dificulty)=> {
+  setInterval(() => {
+    if (ball.y_axis <= computer.y_position + flipper_size/2){
+      computer.y_position -= 1;
+    }
+    if (ball.y_axis >= computer.y_position - flipper_size/2 + ball.size){
+      computer.y_position += 1;
+    }
+  }, 1000/(ball.speed * dificulty));
+  
+}
+
+
 let random_start_direction = ()=> {
   arr = [];
   
@@ -149,8 +165,11 @@ let check_ball_bounce = (ball, flipper_size, screen_height,screen_width,player,
 
 let check_if_ball_touching_wall = (ball, screen_height, screen_width, score)=> {
   if(ball.y_axis >= screen_height || ball.y_axis <= 0) ball_bounce(ball,'y');
-  if(ball.x_axis >= screen_width){score.player++; score_treatment(ball, screen_height, screen_width)}
-  if(ball.x_axis <= 0){score.computer++; score_treatment(ball, screen_height, screen_width)}
+  if(ball.x_axis >= screen_width){score.player++; score_treatment(ball, 
+                                                                  screen_height,
+                                                                  screen_width)}
+  if(ball.x_axis <= 0){score.computer++; score_treatment(ball, screen_height, 
+                                                         screen_width)}
 }
 
 let check_if_ball_touching_flipper = (ball, flipper_size, screen_height, 
@@ -222,8 +241,10 @@ let render_game_screen = ({brush, fps, flipper_size, player, computer, ball,
 
     set_brush_font({brush, font_size: 1.5, font_family: 'Quantico' });
     write_text({brush,  x_axis: 20, y_axis: 30, text: `${fps} fps`});
-    write_text({brush,  x_axis: half_screen_width-150, y_axis: 70, text: `Player: ${score.player}`});
-    write_text({brush,  x_axis: half_screen_width+30, y_axis: 70, text: `Computer: ${score.computer}`});
+    write_text({brush,  x_axis: half_screen_width-150, y_axis: 70,
+                text: `Player: ${score.player}`});
+    write_text({brush,  x_axis: half_screen_width+30, y_axis: 70,
+                text: `Computer: ${score.computer}`});
 
     //game end
     if(score.player == score.max ||
@@ -231,12 +252,15 @@ let render_game_screen = ({brush, fps, flipper_size, player, computer, ball,
 
          clearInterval(in_game);
          set_brush_color(brush,'#081605');
-         draw_rectangle({brush, x_axis:0, y_axis:0, width: screen_width, height: screen_height});
+         draw_rectangle({brush, x_axis:0, y_axis:0, width: screen_width,
+                         height: screen_height});
          set_brush_color(brush,'#FFFFFF');
-         let message = (score.player>score.computer)? 'You win :)!' : ' You lose :(';
-         write_text({brush,  x_axis: screen_width/2-60, y_axis: screen_height/2, text: message });
+         let message = (score.player>score.computer)? 'You win!' : ' You lose';
+         write_text({brush,  x_axis: screen_width/2-60, y_axis: screen_height/2,
+                     text: message });
          message = 'Press F5 to play again';
-         write_text({brush,  x_axis: screen_width/2-120, y_axis: screen_height/2+30, text: message });
+         write_text({brush,  x_axis: screen_width/2-120,
+                     y_axis: screen_height/2+30, text: message });
     }
 
   }, 1000/fps);
